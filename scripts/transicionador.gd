101bf1b770f87c0751a17bd55785572af458c240
neverevis <milan.b.dev@gmail.com>
2026-09-12 13:55:49 -0300

    chore: organiza projeto e corrige arquitetura

--- scripts/transicionador.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-@onready var ap: AnimationPlayer = $AnimationPlayer
-
-func transicionar(caminho : String):
-	ap.play("entrar")
-	await ap.animation_finished
-	get_tree().change_scene_to_file(caminho)
-	ap.play("sair")
