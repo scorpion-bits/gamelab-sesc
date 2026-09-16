c46e46fbc88b6cbca9b9eb564899de905e201014
gisotra <gisotra.henrique@gmail.com>
2026-09-16 15:34:43 -0300

    fix: movimentação das balas corrigida

--- scripts/bala.gd
+++ scripts/bala.gd
@@ -1,15 +1,15 @@
 extends HitboxComponent
 
-var RAPIDEZ = 300
+var RAPIDEZ = 500
 var direcao : Vector2 
 @export var explosao : PackedScene
 
 func _process(delta: float) -> void:
-	global_position = direcao * delta * RAPIDEZ
-
+	global_position += direcao * delta * RAPIDEZ
 
 func _on_area_entered(area: Area2D) -> void:
-	var explosao_instance = explosao.instantiate()
-	explosao_instance.global_position = global_position
-	get_tree().root.add_child(explosao_instance)
-	queue_free()
+	if area is HurtboxComponent:
+		var explosao_instance = explosao.instantiate()
+		explosao_instance.global_position = global_position
+		get_tree().root.add_child(explosao_instance)
+		queue_free()
--- scripts/inimigo.gd
+++ scripts/inimigo.gd
@@ -5,21 +5,27 @@ extends StaticBody2D
 @onready var arma_player: AnimationPlayer = $Bracos/AnimationPlayer
 @export var bala_scene : PackedScene
 @onready var marker_2d: Marker2D = $Bracos/Marker2D
+@onready var recarga: Timer = $Recarga
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
 	if bracos.player_ref != null:
-		var distX = (global_position - bracos.player_ref.global_position).normalized()
+		var distX = global_position.direction_to(bracos.player_ref.global_position) 
 		if distX.x > 0:
-			scale.x = -1
-		else:
 			scale.x = 1
-		atirar()
+		else:
+			scale.x = -1
 
 func atirar():
+	arma_player.play("shoot")
 	var bala_instance = bala_scene.instantiate()
 	bala_instance.global_position = marker_2d.global_position
-	bala_instance.direcao = (marker_2d.global_position - bracos.player_ref.global_position).normalized()
+	bala_instance.direcao = (bracos.player_ref.global_position - marker_2d.global_position).normalized()
+	bala_instance.rotation = bala_instance.direcao.angle()
 	get_tree().root.add_child(bala_instance)
+
+func _on_recarga_timeout() -> void:
+	if bracos.player_ref != null:
+		atirar()
