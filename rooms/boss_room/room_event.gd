24f3d0b6df3c7fef9757bf4c010a7e5fb04ede13
neverevis <milan.b.dev@gmail.com>
2026-09-21 16:29:41 -0300

    fix: arruma a animação do player quando não se move

--- rooms/boss_room/room_event.gd
+++ rooms/boss_room/room_event.gd
@@ -1,7 +1,14 @@
 extends Node
 
-var cutscene_triggered : bool
+var cutscene_triggered : bool = false
+var timer_out : bool = false
 
 func _on_cutscene_trigger_area_body_entered(body: Node2D) -> void:
-	if !cutscene_triggered and body is Player:
-		print("entrou aq")
+	if !cutscene_triggered and body is Player and timer_out:
+		cutscene_triggered = true
+		get_tree().get_first_node_in_group("player").can_move = false
+
+
+func _on_cutscene_trigger_timer_timeout() -> void:
+	timer_out = true
+	print("timer acabous")
