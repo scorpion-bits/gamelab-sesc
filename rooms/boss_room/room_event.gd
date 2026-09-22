e0ea2513b7e21f1f49eac88eb1b849197d6f6610
Thales Miguel Hajes <hajesthales@gmail.com>
2026-09-22 18:02:08 -0300

    vamo

--- rooms/boss_room/room_event.gd
+++ rooms/boss_room/room_event.gd
@@ -16,6 +16,7 @@ var timer_out : bool = false
 func _ready() -> void:
 	trono.monitoring = false
 	trono.interacao = call_lore
+	get_tree().get_first_node_in_group("player").health_component.current_health = 20
 
 
 func call_lore()-> void:
