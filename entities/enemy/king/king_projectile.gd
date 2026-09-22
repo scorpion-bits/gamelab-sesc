93b45729d2b1fcd98e48d0c15f54e5dab8a8cb05
neverevis <milan.b.dev@gmail.com>
2026-09-22 15:40:12 -0300

    refactor: altera a responsabilidade do tempo dos projeteis para a cena do projetil

--- entities/enemy/king/king_projectile.gd
+++ entities/enemy/king/king_projectile.gd
@@ -3,12 +3,26 @@ extends HitboxComponent
 @export var speed : float = 200.0
 @export var direction : Vector2
 
+var bounce_amount : int = 0
+var bounce_count : int = 0
+var duration : float = 2.0
+
+var time : float = 0.0
+
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
+	time += delta
+	
+	if time >= duration:
+		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is HurtboxComponent:
 		queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	if !bounce_amount:
+		queue_free()
+	else:
+		direction = global_position.direction_to(body.global_position)
+		bounce_amount -= 1
