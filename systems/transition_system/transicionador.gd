6e1669102bd75c42b2333e44a2eac79c2fdae790
neverevis <milan.b.dev@gmail.com>
2026-09-24 14:39:43 -0300

    fix: concerta bug onde o jogador invoca múltiplas instancias da próxima sala ao spammar interações com a porta

--- systems/transition_system/transicionador.gd
+++ systems/transition_system/transicionador.gd
@@ -1,12 +1,17 @@
 extends CanvasLayer
 
 @onready var ap: AnimationPlayer = $AnimationPlayer
+var is_running : bool
 
 func change_room(room : PackedScene):
-	ap.play("entrar")
-	await ap.animation_finished
-	
-	get_tree().get_first_node_in_group("room").queue_free()
-	get_tree().get_first_node_in_group("room_container").add_child(room.instantiate())
-	
-	ap.play("sair")
+
+	if !is_running:
+		is_running = true
+		ap.play("entrar")
+		await ap.animation_finished
+		
+		get_tree().get_first_node_in_group("room").queue_free()
+		get_tree().get_first_node_in_group("room_container").add_child(room.instantiate())
+		
+		ap.play("sair")
+		is_running = false
